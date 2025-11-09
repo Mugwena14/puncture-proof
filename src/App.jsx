80503559c240442509b9fd5c6566dd8b66949185
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-11-09 09:58:48 +0200

    Styling restart and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+import PunctureProofPage from './PunctureProofPage'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-background text-foreground">
+      <PunctureProofPage />
+    </div>
+  )
+}
+
+export default App
